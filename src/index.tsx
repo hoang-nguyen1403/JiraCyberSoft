eb83e54383a0cdfc8b8e09d7d0d95a505bb358e0
Hoang Nguyen <hoang.nguyen1403@hcmut.edu.vn>
2022-10-09 14:42:40 +0700

    init folders

---
+++ src/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import "./assets/scss/styles.scss";
+
+
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+<div>
+  hello typescript
+</div>
+);
+
+
